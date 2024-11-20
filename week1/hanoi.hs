94767b157bf8f6998454aeb0509dec1d2e823ab7
Philip Lundström <philip.lundstrom@live.se>
2024-11-20 16:05:00 +0100

    Week 1 done

--- week1/hanoi.hs
+++ week1/hanoi.hs
@@ -1,6 +1,14 @@
 type Peg = String
 type Move = (Peg, Peg)
 
+{-
+ Tower of Hanoi. 
+ n: # of disks
+ a: Peg where all disks start
+ b: Peg destination
+ c: Auxillary Peg
+ -}
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi n a b c 
-    |
+    | n <= 0        = []
+    | otherwise     = hanoi (n-1) a c b ++ [(a, b)] ++ hanoi (n-1) c b a
